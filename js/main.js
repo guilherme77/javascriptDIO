5ad885707f934e818e8fde6f30eafd09bbd91fea
Guilherme Barros <guilhermesilva986@gmail.com>
2020-12-11 16:02:23 -0300

    Add index inicial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = " Guilherme Barros";
+var n2 = 23;
+var n1 = 10;
+var frase = "Japão e o melhor time do mundo"
+
+//alert(nome + " tem " + idade + " anos");
+//alert(n1 + n2) 
+
+console.log(n1+n2);
+console.log(nome);
+console.log(frase.replace("Japão","Brazil"));
